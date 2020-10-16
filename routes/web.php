4e2922308dc5e70431f043ea88cde97ef77cd994
paomnb <pluem393@gmail.com>
2020-10-16 12:05:36 +0700

    report and chart
    16/10/2563

--- routes/web.php
+++ routes/web.php
@@ -179,11 +179,19 @@ Route::get('/product/pdf', 'ProductController@pdf_index');
 Route::resource('profiles', 'profilesController');
 Route::resource('vehicles', 'vehiclesController');
 Route::resource('user', 'UserController');
-Route::resource('order', 'OrderController');
-Route::resource('payment', 'PaymentController');
-Route::resource('order-product', 'OrderProductController');
+
+// Route::resource('order', 'OrderController');
+// Route::resource('payment', 'PaymentController');
+// Route::resource('order-product', 'OrderProductController');
 Route::resource('product', 'ProductController');
+//////////////////////////////////////////////////////////////////////////////////////
 Route::middleware(['auth'])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {    //ONLY ADMIN CAN ACCESS
+        Route::get('order-product/reportdaily', 'OrderProductController@reportdaily');
+        Route::get('order-product/reportmonthly', 'OrderProductController@reportmonthly');
+        Route::get('order-product/reportyearly', 'OrderProductController@reportyearly');
+    });
+
 Route::resource('order', 'OrderController');
 Route::resource('payment', 'PaymentController');
 Route::resource('order-product', 'OrderProductController');
